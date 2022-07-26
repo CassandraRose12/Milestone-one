738e67070d01007b459f6e244c9e2e344a6d83fa
Cassandra Collins <cassandracollins@Cassandras-MacBook-Air.local>
2022-07-26 18:59:14 -0400

    New JS

--- trivia.js
+++ trivia.js
@@ -1,5 +1,21 @@
 //code multiple choice drink questions and answers
-
 //code fill-in questions and answers
 //source simplestepcode.com and "A Smarter Way to Learn Javascript" By Mark Myers
+// code to select the quiz elements
+
+const quizContiner = document.getElementById('quiz');
+const resultsConatiner = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+//functions to build quiz
+function buildQuiz (){}
+
+function showResults(){}
+
+buildQuiz ();
+
+submitButton.addEventListener('click', showResults);
+
+//Questions and Answers
+
 
